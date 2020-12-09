b9e1987ded6f3a7d33912cf14b2a90e79bcda4e2
zotacy <tacyzacharyo@johndeere.com>
2020-12-08 23:45:20 -0500

    Setup initial components

---
+++ src/components/Hompage/Homepage.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+// import {Link} from 'react-router-dom';
+
+class HomePage extends Component{
+    render(){
+        return(
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    }
+}
+
+export default HomePage;
---
+++ src/components/PlayerProfile/PlayerProfile.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+// import {Link} from 'react-router-dom';
+
+class PlayerProfile extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Player Profile</h1>
+            </div>
+        )
+    }
+}
+
+export default PlayerProfile;
