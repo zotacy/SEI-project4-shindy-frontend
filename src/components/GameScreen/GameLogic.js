de0bffb87f43cc5e4438a12ae99921fd9817d708
zotacy <tacyzacharyo@johndeere.com>
2020-12-14 14:37:50 -0500

    Game Logic: Turns work, attack works

--- src/components/GameScreen/GameLogic.js
+++ src/components/GameScreen/GameLogic.js
@@ -9,7 +9,7 @@ class GameLogic extends Component{
         let counter = 0;
         
         //Cell Array, addEventListeners & cellClick function
-        function cellClick(){
+        turnLogic=()=>{
             if (turn == 'comp' && this.classList.contains('clicked') == false){
                 this.classList.add('clicked')
                 moves[this.id] = turn;
@@ -25,8 +25,16 @@ class GameLogic extends Component{
             };
         };
 
-        function winLogic(){
-            if (
+        winLogic=()=>{
+            if (this.props.player.hp === 0){
+                alert("You won")
+                // this.props.endGame()
+            } else if (this.props.computer.hp === 0){
+                alert("You have been defeated")
+                // this.props.endGame()
+            } else {
+                // this.props.nextTurn()
+            }
         };
     }
 }
--- src/components/GameScreen/GameScreen.js
+++ src/components/GameScreen/GameScreen.js
@@ -16,7 +16,6 @@ class GameScreen extends Component{
                 defense: 5,
                 recover: 10,
                 spd: 1,
-
             },
             computer:{
                 title:"Computer",
@@ -28,67 +27,119 @@ class GameScreen extends Component{
                 recover: 10,
                 spd: 1,
             },
-            turn:'',
-            currentTurn:{},
+            playerTurn: null,
+            moves:[],
+            disabled:false,
+            // currentTurn:{},
         }
     }
     //Game Logic
     playGame=()=>{
         alert("Battle Begin!")
         console.log("Battle Begin!")
+        let player= this.state.player;
+        let computer= this.state.computer; 
+        let moves = this.state.moves;
+        if (player.spd === computer.spd && moves.length==0){
+            console.log('speeds match, moves 0')
+            let x=Math.floor(Math.random()*2)
+            if (x===0) {
+                console.log('x= 0')
+                this.setState({playerTurn: false})
+            }
+            else {
+                this.setState({playerTurn: true})
+            }
+        }
+        this.nextTurn()
     }
+    
     // Game Methods
     attack=()=>{
-        console.log('Attack!')
+        if (this.state.playerTurn === true){
+            let compObj= this.state.computer
+            compObj.hp = compObj.hp - this.state.player.attack
+            this.setState({
+                computer: compObj
+            });
+            let action =`You dealt ${this.state.player.attack} damage`
+            console.log(action)
+            this.setState({moves: [...this.state.moves, action]})
+            this.nextTurn()
+        } else {
+            let playerObj= this.state.player
+            playerObj.hp = playerObj.hp - this.state.computer.attack
+            this.setState({
+                player: playerObj
+            });
+            let action=`You took ${this.state.computer.attack} damage`
+            console.log(action)
+            this.setState({moves: [...this.state.moves, action]})
+            this.nextTurn()
+        }
     }
     block=()=>{
-        console.log('Block')
+        if (this.state.playerTurn === true){
+            console.log(`You blocked ${this.state.player.defense*2} damage`)
+        } else {
+            console.log(`Opponent blocked some of your damage`)
+        }
     }
     recover=()=>{
-        console.log(`Recovered hp`)
+        if (this.state.playerTurn === true){
+            console.log(`You recovered ${this.state.player.recover} hp`)
+        } else {
+            console.log(`Your opponent recovered ${this.state.computer.recover} hp`)
+        }
     }
     trickery=()=>{
-        console.log('...Trickery has occured...')
+        const trickStats= ['hp,attack,defense,recover,spd'];
+        let trickStat= trickStats[Math.floor(Math.random()*trickStats.length)]
+        if (this.state.playerTurn === true){
+            console.log(`You performed some trickery... ${trickStat} increased`)
+        } else {
+            console.log(`Your opponent is getting into some mischief`)
+        }
     }
     updateHp=()=>{
         console.log(`${this.state.title}`)
     }
+    nextTurn=()=>{ // Infinite Loop Error when using this.setState
+        let player= this.state.player;
+        let computer= this.state.computer; 
+        let moves = this.state.moves;
+        console.log(`${this.state.playerTurn}'s turn`)
+        if (this.state.playerTurn === false){
+            console.log('comp turn else if')
+            this.setState({playerTurn: !this.state.playerTurn})
+        } else if (this.state.playerTurn === true){
+            console.log('player turn else if')
+            this.setState({playerTurn: !this.state.playerTurn})
+        }
+        // (player.spd > computer.spd) && (moves[moves.length-1]=='comp' || moves[moves.length-1]==null)){
+    }
+    
     // Render Game Screen
     render(){
         let player= this.state.player;
         let computer= this.state.computer; 
         let currentTurn= this.currentTurn;
-        // Infinite Loop Error: when using this.setState
-        if (player.spd === computer.spd){
-           let x=Math.floor(Math.random()*2)
-           if (x===0) {this.state.turn='comp'}
-           else {this.state.turn='player'} 
-        } else if (player.spd > computer.spd){
-            this.state.turn='player'
-        } else {
-            this.state.turn='comp'
-        }
-        if (this.state.turn ==='player'){
-            this.state.currentTurn= this.state.player
-        } else {
-            this.state.currentTurn= this.state.computer
-        }
+        // this.nextTurn()
         console.log(this.state)
-        console.log(this.state.turn) 
-        // console.log(this.currentTurn.title)
+        // console.log(this.state.turn)
         return(
             <main>
                 {/* <!-- Player --> */}
                 <div className="playerbox" id="player1">
                     <div className="char" >
                         <h2>{player.title}</h2>
-                        <div className="hpBar" hp="" maxhp="100"></div>
+                        <div className="hpBar" hp="" maxhp={this.state.player.hp}></div>
                     </div>
                     <div className="actionbox">
-                        <button className="action" id="attack" onClick={this.attack}>Attack!</button>
-                        <button className="action" id="defend" onClick={this.block}>Block</button>
-                        <button className="action" id="recover" onClick={this.recover}>Recover</button>
-                        <button className="action" id="trick" onClick={this.trickery}>Trickery</button>
+                        <button className="action" id="attack" onClick={this.attack} disabled={this.state.disabled}>Attack!</button>
+                        <button className="action" id="defend" onClick={this.block} disabled={this.state.disabled}>Block</button>
+                        <button className="action" id="recover" onClick={this.recover} disabled={this.state.disabled}>Recover</button>
+                        <button className="action" id="trick" onClick={this.trickery} disabled={this.state.disabled}>Trickery</button>
                     </div>
                 </div>
                     {/* <!-- Middle Space --> */}
@@ -99,7 +150,7 @@ class GameScreen extends Component{
                 <div className="playerbox" id="playerC">
                     <div className="char" >
                         <h2>{computer.title}</h2>
-                        <div className="hpBar" hp="" maxhp="100"></div>
+                        <div className="hpBar" hp="" maxhp={this.state.computer.hp}></div>
                     </div>
                     <div className="actionbox" >
                         <button className="action" id="attack" >Attack!</button>
