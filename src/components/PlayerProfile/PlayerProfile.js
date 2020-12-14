29014e7fd1ac323d39517247e3ec3a8824842102
zotacy <tacyzacharyo@johndeere.com>
2020-12-13 23:53:23 -0500

    refactor profile & chacracter blocks in PlayerProfile

--- src/components/PlayerProfile/PlayerProfile.js
+++ src/components/PlayerProfile/PlayerProfile.js
@@ -3,9 +3,9 @@ import React, {Component} from 'react';
 import './PlayerProfile.css';
 
 class PlayerProfile extends Component{
-    // constructor(props){
-    //     super(props)
-    //     playerDetails={
+    constructor(props){
+        super(props)
+    //     this.state={
     //         // username: this.props.user.username,
     //         userId: this.props.user.id,
     //         name: this.props.user.name,
@@ -14,7 +14,8 @@ class PlayerProfile extends Component{
     //         password: this.props.user.password,
     //         characters: this.props.user.characters,
     //     }
-    // }  
+    }
+
     render(){
         const playerDetails= this.props.user;
         console.log(playerDetails)
@@ -35,26 +36,28 @@ class PlayerProfile extends Component{
         console.log(characters)
         
         return(
-            <main className="profile">
+            <main className="profileBlock">
               <div className="playerInfo">
-                <h2>{playerDetails.username}</h2>
+                <h1>{playerDetails.username}</h1>
                 <form onSubmit={this.props.updateProfile}>
                     <input id='userId' type="hidden" name="userId" value={playerDetails.userId} /><br/>
-                    Name:<input id='name' type="text" name="name" placeholder={playerDetails.name} /><br/>
-                    Email:<input id='email' type="text" name="email" placeholder={playerDetails.email} /><br/>
-                    Username:<input id='username' type="text" name="username" placeholder={playerDetails.username} /><br/>
-                    Password:<input id='password' type="password" name="password" placeholder='******'/><br/>
+                    <input id='name' type="text" name="name" placeholder={playerDetails.name} /><br/>
+                    <input id='email' type="text" name="email" placeholder={playerDetails.email} /><br/>
+                    <input id='username' type="text" name="username" placeholder={playerDetails.username} /><br/>
+                    <input id='password' type="password" name="password" placeholder='Update Password'/><br/>
                     <br/>
-                    <input type="submit" value="Edit Profile" />
+                    <input type="submit" value="Update Profile" />
                 </form>
                 <form onSubmit={this.props.deleteProfile}>
                     <input type="submit" value="Delete Profile" />
                 </form>
 
                 </div>
-                <div className="characters">
+                <div className="charactersBlock">
                     <h1>Player Characters</h1>
-                    {characters}
+                    <div className="characters">
+                        {characters}
+                    </div>
                 </div>
             </main>
         )
